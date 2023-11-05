b4b0de47d33766b879c9c95bc84a63eec2e5d058
mahbub-repliq <mahbubul.alam@repliq.dev>
2023-11-06 00:13:08 +0600

    necessary comment added to make code more understandable

--- components/Gallery.js
+++ components/Gallery.js
@@ -14,26 +14,31 @@ import ImageUploader from "./ImageUploader";
 const inter = Inter({ subsets: ["latin"] });
 
 const Gallery = () => {
+  // State variables
   const [selectThumbnails, setSelectThumbnails] = useState([]);
   const [thumbnails, setThumbnails] = useState(images);
   const [dragging, setDragging] = useState(false);
   const [draggedImage, setDraggedImage] = useState(null);
   const [draggedIndex, setDraggedIndex] = useState(null);
 
+  // Handle the start of image drag
   const handleDragStart = (image) => {
     setDragging(true);
     setDraggedImage(image);
   };
 
+  // Handle drag over an element
   const handleDragOver = (e) => {
     e.preventDefault();
     e?.target?.children[0]?.alt && setDraggedIndex(e?.target?.children[0]?.alt);
   };
 
+  // Handle the drop of an image
   const handleDrop = (index) => {
     setDragging(false);
 
     if (draggedImage) {
+      // Create a new array of images with the dragged image in the specified index
       const updatedImages = thumbnails.filter(
         (image) => image.id !== draggedImage.id
       );
@@ -44,7 +49,9 @@ const Gallery = () => {
     }
   };
 
+  // Handle the click event for deleting selected images
   const handleDeleteClick = () => {
+    // Filter out selected thumbnails from the list
     const updatedImages = thumbnails.filter(
       (image) => !selectThumbnails.some((selected) => selected.id === image.id)
     );
@@ -53,9 +60,11 @@ const Gallery = () => {
     setSelectThumbnails([]);
   };
 
+  // Handle file upload
   const handleFileUpload = (e) => {
     const selectedFiles = e.target.files;
 
+    // Generate new images from selected files
     const newImages = Array.from(selectedFiles).map((file, index) => {
       const id = thumbnails.length + index + 1;
       const thumbnail = URL.createObjectURL(file);
@@ -63,9 +72,9 @@ const Gallery = () => {
       return { id, thumbnail };
     });
 
+    // Add the new images to the existing thumbnails
     setThumbnails([...thumbnails, ...newImages]);
   };
-
 
   return (
     <main
